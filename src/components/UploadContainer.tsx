f57492a01c9fa8aeb7927b1f75c216dbefda6671
umangcodes <umanga.eng@gmail.com>
2023-09-28 11:16:18 -0400

    step 1 api call

--- src/components/UploadContainer.tsx
+++ src/components/UploadContainer.tsx
@@ -218,7 +218,7 @@ const UploadImage = ({ model, onSuccess, step1Data }: any) => {
                   image: imgSrc,
                 });
                 setResults(res.data);
-
+                console.log(res.data.heathcard)
                 onSuccess({ ...res.data, healthCardImage: imgSrc })
                 setLoading(false);
               } else {

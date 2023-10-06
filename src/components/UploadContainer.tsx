ab1a47893a806255e801feeea78e659c8cd44c17
umangcodes <umanga.eng@gmail.com>
2023-10-06 11:27:12 -0400

    feat: mode the api call ahead of setting the data in context

--- src/components/UploadContainer.tsx
+++ src/components/UploadContainer.tsx
@@ -551,8 +551,6 @@ const UploadContainer = ({ step1Data, setStep1Data }: any) => {
     data.issueDate = data.issueDate.toString().slice(0, 4) + "-" + data.issueDate.toString().slice(4, 6) + "-" + data.issueDate.toString().slice(6,)
     data.expiryDate = data.expiryDate.toString().slice(0, 4) + "-" + data.expiryDate.toString().slice(4, 6) + "-" + data.expiryDate.toString().slice(6,)
     data.dob = data.dob.toString().slice(0, 4) + "-" + data.dob.toString().slice(4, 6) + "-" + data.dob.toString().slice(6,)
-    setStep1Data(data);
-    console.log(sanitize(data.healthcard))
     await axios.post("https://us-central1-patient-registration-portal.cloudfunctions.net/web/newVisit", {healthcard: sanitize(data.healthcard).slice(0,10), location: "101"}).then(async (resp) => {
       if(resp.data.msg == "visit created"){
         window.localStorage.setItem("token", resp.data.token)
@@ -561,6 +559,7 @@ const UploadContainer = ({ step1Data, setStep1Data }: any) => {
         console.log("error occured.")
       }
     }, err => {console.log(err.message)})
+    setStep1Data(data);
   }
 
   return (

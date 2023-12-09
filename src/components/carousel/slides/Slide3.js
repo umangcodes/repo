ee513c0ffa7fb8b5e783fba9d6fd09381326d50b
umangcodes <umanga.eng@gmail.com>
2023-12-09 12:56:32 -0500

    feat: link update

--- src/components/carousel/slides/Slide3.js
+++ src/components/carousel/slides/Slide3.js
@@ -2,7 +2,7 @@ function Slide3() {
   return (
     <div className="flex flex-col">
         <h1 className="text-xl text-center py-5">Skip queues & waiting</h1>
-        <h2 className="text-center text-blue-800 underline"><a href="https://www.mhlab.ca/patient-result-portal-registration-request/">Book a home visitation today!</a></h2>
+        <h2 className="text-center text-blue-800 underline"><a href="https://www.mhlab.ca/home-lab-visit/">Book a home visitation today!</a></h2>
     </div>
   )
 }

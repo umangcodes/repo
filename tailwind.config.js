5b4367663eee0a0f9fd7773774b688fc66e5ec03
umangcodes <umanga.eng@gmail.com>
2023-12-19 16:06:58 -0500

    tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
         primary: "#00ABF4",
         secondary: "#15243F",
         background: "#FBFAFC",
-        disabled: '#E8E8E8'
+        disabled: '#E8E8E8',
+        secondaryShade: "#737c8c"
       },
       fontFamily: {
         'oswald': ['Oswald', 'sans-serif'],
@@ -18,6 +19,9 @@ module.exports = {
       fontSize:{
         "min": "10px"
       },
+      boxShadow:{
+        'special': '0 1px 2px 0 rgba(0, 171, 244, 0.1), 0 1px 2px 0 rgba(0, 171, 244, 0.2)'
+      }
       // backgroundImage:{
       //   'checkmark': "url('/src/assets/images/checkmark.gif')"
       // }

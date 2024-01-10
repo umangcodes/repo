668451df1e5fca14e9eb22d9217a24a278ca618d
umangcodes <umanga.eng@gmail.com>
2024-01-10 10:15:27 -0500

    submit page issue debug

--- src/pages/steps/Stepa5.js
+++ src/pages/steps/Stepa5.js
@@ -14,8 +14,8 @@ function Stepa5() {
   }, [newContextValue]);
   return (
     <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{duration: 0.5}}>
-      {newContextValue.pages.step1 ? "" : <p className="border text-center text-xl font-semibold rounded-xl w-[40%] shadow-sm text-red-600 mb-5">Error at step 1 or 2</p> }
-      {newContextValue.pages.step4 ?  "" : <p className="border text-center text-xl font-semibold rounded-xl w-[40%] shadow-sm text-red-600">Error at step 4</p>}
+      {newContextValue.pages.step1 && newContextValue.pages.step4 ? "" : <p className="border text-center text-xl font-semibold rounded-xl w-[40%] shadow-sm text-red-600 mb-5">Please check the form something is wrong</p> }
+      
     </motion.div>
   )
 }

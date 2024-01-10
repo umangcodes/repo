81782024324b52767b24e75d8b04a8f393323727
umangcodes <umanga.eng@gmail.com>
2024-01-10 09:44:13 -0500

    debugging address issue

--- src/pages/steps/Stepa3.js
+++ src/pages/steps/Stepa3.js
@@ -57,6 +57,10 @@ function Stepa3() {
       postalCode: postalCode
     })
   },[postalCode])
+
+  useEffect(()=>{
+    console.log(addressInput)
+  },[addressInput])
   return (
     <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{duration: 0.5}}>
       <div id="address-field" className="flex flex-col">
